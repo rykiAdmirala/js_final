09e034e305cb031b8870fb660dd772f4e8886ef3
D M <ryki.admirala@gmail.com>
2017-02-21 17:49:39 +0200

    mobile styles done

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,21 @@ var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     livereload = require('gulp-livereload'),
     image = require('gulp-image'),
-    spritesmith = require('gulp.spritesmith');
+    spritesmith = require('gulp.spritesmith'),
+    cache = require('gulp-cache'),
+    del = require('del');
 
+/* Copying HTML */
+gulp.task('html', function() {
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('www'))
+    .pipe(livereload());
+})
+
+/* Making sprite */
 gulp.task('sprite', function () {
   var spriteData = gulp.src('src/img/sprite/*.*').pipe(spritesmith({
+    padding: 2,
     imgName: 'sprite.png',
     cssName: '_sprite.scss'
   }));
@@ -15,12 +26,14 @@ gulp.task('sprite', function () {
   return spriteData.css.pipe(gulp.dest('src/css/'));
 });
 
+/* Optimizing images */
 gulp.task('image', function () {
   return gulp.src('src/img/*.*')
-    .pipe(image())
+    .pipe(cache(image()))
     .pipe(gulp.dest('www/img/'));
 });
 
+/* Compiling SASS */
 gulp.task('sass', function () {
  return gulp.src('src/css/*.scss')
   .pipe(sourcemaps.init())
@@ -30,9 +43,20 @@ gulp.task('sass', function () {
   .pipe(livereload());
 });
 
+
 gulp.task('sass:watch', function () {
   livereload.listen();
+  gulp.watch('src/index.html', gulp.series('html'));
   return gulp.watch('src/css/*.scss', gulp.series('sass'));
 });
 
-gulp.task('default', gulp.series('sprite', 'image', 'sass', 'sass:watch'));
+/* Cleaning */
+gulp.task('clean', function() {
+  return del('www');
+})
+
+
+gulp.task(
+  'default',
+  gulp.series('clean', 'html', 'sprite', 'image', 'sass', 'sass:watch')
+);
